0ba04507ff51491d85b7302dc6ce253bb619954c
vivek kushwaha <vivekkushwaha@viveks-MacBook-Air.local>
2024-05-26 13:43:43 +0530

    Whitelist allowed host list

--- newsletter_service/newsletter_service/settings.py
+++ newsletter_service/newsletter_service/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+^$g$yc$z!$s90x694owlahe01%vg-#@azuy(_=#jeum)(zm*t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
